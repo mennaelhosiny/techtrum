2d705d0f243675a44e24225ac285c7c29b2b94ba
mennaelhosiny <mennaelhosiny3@gmail.com>
2025-05-28 00:52:42 +0200

    updated

--- js/translate.js
+++ js/translate.js
@@ -72,15 +72,15 @@ document.addEventListener('DOMContentLoaded', () => {
       networkser: "Network Performance",
 
 
-      titleser1: "Spectrum Analysis",
+      titleser1: "Telecommunications & Smart Cities",
       descriptionser1: "Advanced frequency spectrum analysis and optimization compliant with ITU and MCIT regulations",
 
 
-      titleser2: "Fiber Optics Networks",
+      titleser2: "Digital transformation ",
       descriptionser2: "SDN-controlled fiber optics networks with QKD for secure communications infrastructure",
 
 
-      titleser3: "Smart City Blueprints",
+      titleser3: "Emerging Technologies & Innovation ",
       descriptionser3: "Digital twin solutions integrating IoT, satellites, and sustainable urban development",
 
 
@@ -111,13 +111,32 @@ document.addEventListener('DOMContentLoaded', () => {
       titleser10: "Capacity Planning",
       descriptionser10: "Data traffic studies and network expansion strategies for future growth",
 
-
-
       viewAll: "View All Services",
       readMore: "Read More",
       subTitle5: "OUR TEAM",
-      title5: "Generating Ideas is Teamwork"
-
+      title5: "Generating Ideas is Teamwork",
+      contactser1:"Wireless and Fiber Connectivity Advisory",
+      contactser1desc:"we are specialized in consultancy of wide range of spectrum analysis, coverage mapping, optimal antenna placement, Network CAPEX/OPEX for wireless deployments, IBS Design, Radio Frequency design and Optimization, critical mission, feasibility Studies & Route Planning, OSP Engineering, 5G Backhaul Solutions, SDN-Controlled Fiber Networks, Quantum Key Distribution (QKD), all of these services and consultancy within ensuring adherence to ITU, MCIT and CST regulations.",
+      contactser2:"Smart City Strategies & Planning ",
+      contactser2desc:"with Techtrum Consult you can Define long-term smart city goals aligned with sustainability, efficiency, and citizen well-being, Assess current infrastructure and identify areas for smart transformation, Digital Twins, Satellite, IoT and best practice Connectivity.",
+      contactser3:"Network Performance",
+      contactser3desc:"Our capabilities in Benchmarking and Performance Optimization Strategies for Network Capacity Planning, Scalability, RF Interference Mitigation, Band Steering, Load Balancing and 5G Network Slicing Optimization can scale up your growth on very tied time and take your level into next jump. ",
+      contactser11:"Digital Transformation Consultation.",
+      contactser11desc:"We are experienced for advisory services such as Digital Strategy, Roadmap creation & Development. Therefore with Techtrum Consult can take your level up through Automation & Process Optimization and then customer experience (CX) with Digital Engagement while ensuring the quality of our consultancy through training & change management programs. ",
+      contactser22:"Digital Transformation solution and software ",
+      contactser22desc:"Techtrum Consult explored and landscaping of Digital Transformation solutions on wide range locally and globally, so we built a partnership for the best solutions can be provided with Zero code, such as Origami solution and Simplycast solution.",
+      contactser33:"Data Management Consultations",
+      contactser33desc:"Techtrum Consult having a great team specialized in data management, we providing Data management Strategy & Governance, edge Data Management solutions and consultation.",
+      contactser44:"IT Governance Consulting Services",
+      contactser44desc:"Techtrum Consult possesses the expertise to deliver comprehensive IT governance services, providing technical governance consulting, risk management, compliance advisory, and implementation of best practices in digital infrastructure. We specialize in applying international standards such as ISO 31000, ISO27001, ISO38500, COBIT and ITIL",
+      contactser111:"Artificial intelligence & Machine Learning Innovation",
+      contactser111desc:"Techtrum Consult providing the consultation in Automation Strategy, AI/ML studies and development.",
+      contactser222:"Metaverse & Extended Reality (XR)",
+      contactser222desc:"Extending to our capabilities and experience in emerging technologies, we providing AR/VR Strategy, -VR, AR Applications, digital twins simulations and development of Metaverse Platforms Development",
+      contactser333:"Sustainable & Green Tech Studies",
+      contactser333desc:"We believe in our positive impact in ESG reporting, public polices, impact assessment and on solution side we providing the emission tracking solutions and Green AI & IoT Ecosystems solutions.",
+      contactser444:"Innovation Consultation",
+      contactser444desc:"Our Specialized consultation in innovation focusing on Research & Development, Disruptive Tech Adoption, Prototyping & MVP Development, Climate Tech Advisory, Innovation Strategy Blueprint and Proof of Concept (PoC) Development",
     },
     ar: {
       home: "الرئيسية",
@@ -187,13 +206,13 @@ document.addEventListener('DOMContentLoaded', () => {
       smartCitiesser: "المدن الذكية",
       networkser: "أداء الشبكة",
 
-      titleser1: "تحليل الطيف الترددي",
+      titleser1: "  الاتصالات والمدن الذكية",
       descriptionser1: "تحليل وتحسين متقدم للطيف الترددي وفقاً لأنظمة الاتحاد الدولي للاتصالات ووزارة الاتصالات",
 
-      titleser2: "شبكات الألياف الضوئية",
+      titleser2: "التحول الرقمي",
       descriptionser2: "شبكات ألياف ضوئية متحكم بها برمجياً مع توزيع المفاتيح الكمية للبنية التحتية الآمنة",
 
-      titleser3: "خطط المدن الذكية",
+      titleser3: "التقنيات الناشئة والابتكار",
       descriptionser3: "حلول التوأم الرقمي التي تدمج إنترنت الأشياء والأقمار الصناعية مع التطور الحضري المستدام",
 
 
@@ -219,10 +238,30 @@ document.addEventListener('DOMContentLoaded', () => {
       titleser9: "تحسين الترددات الراديوية",
       descriptionser9: "تقنيات متقدمة لتقليل التداخل وزيادة كفاءة الطيف الترددي",
 
-
       titleser10: "تخطيط السعات",
-      descriptionser10: "دراسات حركة البيانات واستراتيجيات توسعة الشبكة للنمو المستقبلي"
-
+      descriptionser10: "دراسات حركة البيانات واستراتيجيات توسعة الشبكة للنمو المستقبلي",
+      contactser1:"الاستشارات في الاتصالات اللاسلكية والألياف الضوئية",
+      contactser1desc:"نختص في استشارات تحليل الطيف الترددي، رسم خرائط التغطية،  تحديد المواقع المثالية للهوائيات، تحليل التكاليف الرأسمالية والتشغيلية للتغطية اللاسلكية، تصميم أنظمة المباني الذكية IBS))، تصميم وتحسين الترددات الراديوية، دراسات الجدوى وتخطيط حركة البيانات والاتصالات، هندسة الشبكات الخارجية OSP))، حلول النقل الشبكي للجيل الخامس، شبكات الألياف الضوئية المتحكم بها برمجيًا SDN))، توزيع المفاتيح الكمية QKD))، كل ذلك مع الالتزام بأنظمة الاتحاد الدولي للاتصالاتITU))  ووزارة الاتصالات وتقنية المعلوماتMCIT))  وهيئة الاتصالات والفضاء والتقنيةCST).)",
+      contactser2:"الاستشارات في المدن الذكية",
+      contactser2desc:"مع تيكتروم كونسلت يمكنك تحديد أهداف المدن الذكية طويلة المدى المواكبة للاستدامة والكفاءة ورفع جودة الحياة، وتقييم البنية التحتية الحالية وتحديد مجالات التحول الذكي، بما في ذلك التوئم الرقمي، الأقمار الصناعية، إنترنت الأشياء وأفضل الحلول التقنية الحديثة.",
+      contactser3:"أداء الشبكة",
+      contactser3desc:"قدراتنا في تقييم الأداء وتحسين استراتيجيات أداء الشبكات اللاسلكية وشبكات الألياف الضوئية، تخطيط السعات، التوسع، تخفيف تداخل الترددات الراديوية، توجيه النطاقات، موازنة الحمل وتحسين تقطيع شبكات الجيل الخامس يمكن أن تعزز نموك في وقت قياسي وتأخذ منظمتك إلى قفزات نوعية.",
+      contactser11:"استشارات التحول الرقمي",
+      contactser11desc:"لدينا خبرة كبيرة في خدمات الاستشارات مثل الاستراتيجية الرقمية، إنشاء وتطوير خطط العمل. يمكننا تقديم استشاراتنا للإرتقاء بمستوى المنظمات من خلال أتمتة وتحسين العمليات ثم تجربة العملاءCX)) مع الموائمة الرقمية وضمان جودة استشاراتنا من خلال برامج التدريب وإدارة التغيير",
+      contactser22:"حلول وبرمجيات التحول الرقمي",
+      contactser22desc:"توسعت تيكتروم كونسلت في رسم خريطة شاملة لحلول التحول الرقمي على نطاق واسع محليًا وعالميًا، لذلك قمنا ببناء شراكات لأفضل الحلول التي يمكن تقديمها بدون أي جهود لكتابة الأكواد البرمجية، مثل برنامج أوريغامي و سيمبلي كاست.",
+      contactser33:"الاستشارات في إدارة البيانات",
+      contactser33desc:"لدينا في تيكتروم كونسلت فريق رائع متخصص في إدارة البيانات، نقدم الدراسات اللإستشارية في استراتيجيات إدارة البيانات والحوكمة، ونقدم أيضاً حلول إدارة البيانات الطرفية.",
+      contactser44:"الاستشارات في حوكمة تقنية المعلومات",
+      contactser44desc:"لدى تكتروم كونسلت الخبرة الكافية للقيام بأعمال الحوكمة في تقنية المعلومات وتقديم الاستشارات في الحوكمة التقنية وإدارة المخاطر والالتزام وتبني افضل الممارسات في البنية التحتية الرقمية وتطبيق المعايير الدولية مثل ISO31000, ISO27001, ISO38500, COBIT, ITIL، والقيام بتحديد مؤشرات الآداء KPIs",
+      contactser111:"الذكاء الاصطناعي وتعلم الآله",
+      contactser111desc:"نقدم في تيكتروم كونسلت الاستشارات في حلول واستراتيجيات الأتمتة، دراسات وتطوير الذكاء الاصطناعي والتعلم الآلي.",
+      contactser222:"استشارات الميتافيرس والواقع المعززXR))",
+      contactser222desc:"امتدادًا لقدراتنا وخبراتنا في التقنيات الناشئة، نقدم الاستشارات المتخصصة في الواقع الافتراضي والمعزز، و تطبيقات الواقع الافتراضي والمعزز، ومحاكاة التوائم الرقمية وتطوير منصات الميتافيرس.",
+      contactser333:"الدراسات الاستشارية في التقنيات والإستدامة",
+      contactser333desc:"نؤمن بتأثيرنا الإيجابي في تقديمنا لتقارير متخصصة في الحوكمة البيئية الاجتماعية ESG، السياسات العامة، تقييم الأثر ، وعلى جانب الحلول نقدم حلول تتبع الانبعاثات الكربونية وحلول النظم البيئية للذكاء الاصطناعي وإنترنت الأشياء (Ecosystem).",
+      contactser444:"الخدمات الاستشارية في أعمال الابتكار",
+      contactser444desc:" نركز في استشاراتنا المتخصصة في الابتكار على البحث والتطوير، تبني التقنيات المبتكرة، تطوير النماذج الأولية والحد الأدنى من المنتجات القابلة للتطبيق MVP))، الاستشارات في تقنيات المناخ، مخططات استراتيجية الابتكار وتطوير نماذج اثبات المفاهيم PoC).)",
     }
   };
 
