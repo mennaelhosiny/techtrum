a8b3e1c4c8eebb9d5e8cfd7d68daa98f51d1dcff
mennaelhosiny <mennaelhosiny3@gmail.com>
2025-06-12 00:29:01 +0200

    updated

--- main.php
+++ main.php
@@ -1,26 +1,19 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // البيانات من الفورم
-    $name    = htmlspecialchars($_POST['name']);
     $email   = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
     $message = htmlspecialchars($_POST['message']);
 
-    // إعدادات الإيميل
-    $to      = "mrnnaelhosiny3@gmail.com";
-    $subject = "رسالة جديدة من تكتروم";
-    $body    = "الاسم: $name\n";
-    $body   .= "البريد الإلكتروني: $email\n\n";
-    $body   .= "الرسالة:\n$message";
+    $to = "mennaelhosiny3@gmail.com";
+    $body = "From: $email\n\nMessage:\n$message";
 
-    $headers  = "From: $email\r\n";
+    $headers = "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/plain; charset=utf-8\r\n";
 
-    // إرسال الإيميل
     if (mail($to, $subject, $body, $headers)) {
         echo "تم إرسال الرسالة بنجاح.";
     } else {
-        echo "حدث خطأ أثناء الإرسال، حاول مرة أخرى.";
+        echo "حدث خطأ أثناء الإرسال.";
     }
 } else {
     echo "طلب غير صحيح.";
